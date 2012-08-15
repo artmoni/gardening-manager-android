c220095e3b6d59f84a521a1ae312328a200c5b15
sebastien.fleury <sebastien.fleury@8d69d5cf-e2a8-420a-b999-7e637039f93c>
2012-08-15 21:05:12 +0000

    git-svn-id: http://www.jongle-ris.org/svn/GardeningOverTheSun/branch/br0.10@991 8d69d5cf-e2a8-420a-b999-7e637039f93c

--- src/org/gots/preferences/GotsPreferences.java
+++ src/org/gots/preferences/GotsPreferences.java
@@ -2,5 +2,5 @@ package org.gots.preferences;
 
 public class GotsPreferences {
 	public static final String ANALYTICS_ID = "UA-916500-18";
-	public static boolean DEVELOPPEMENT = false;
+	public static boolean DEVELOPPEMENT = true;
 }
