039987fc432cda36f915ced38e031bb54b6fba17
sebastien.fleury <sebastien.fleury@8d69d5cf-e2a8-420a-b999-7e637039f93c>
2013-01-21 00:27:50 +0000

    Asynchronouse update seed
    git-svn-id: http://www.jongle-ris.org/svn/GardeningOverTheSun/branch/br0.10@1141 8d69d5cf-e2a8-420a-b999-7e637039f93c

--- src/org/gots/garden/GardenManager.java
+++ src/org/gots/garden/GardenManager.java
@@ -1,11 +1,13 @@
 package org.gots.garden;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.gots.DatabaseHelper;
 import org.gots.garden.sql.GardenDBHelper;
 import org.gots.seed.BaseSeedInterface;
+import org.gots.seed.adapter.ListVendorSeedAdapter;
 import org.gots.seed.providers.GotsConnector;
 import org.gots.seed.providers.local.LocalConnector;
 import org.gots.seed.providers.simple.SimpleConnector;
@@ -14,12 +16,15 @@ import org.gots.weather.WeatherManager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.widget.Toast;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 public class GardenManager {
 	private SharedPreferences preferences;
 	private Context mContext;
+	private boolean isLocalStore = false;
 
 	public GardenManager(Context mContext) {
 		this.mContext = mContext;
@@ -34,7 +39,7 @@ public class GardenManager {
 
 		changeDatabase((int) newGarden.getId());
 
-		populateVendorSeed(false);
+		new RefreshTask().execute(new Object());
 
 		return newGarden.getId();
 	}
@@ -45,11 +50,12 @@ public class GardenManager {
 
 		changeDatabase((int) newGarden.getId());
 
-		populateVendorSeed(localStore);
+		isLocalStore = localStore;
+		new RefreshTask().execute(new Object());
 
 		GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
 		tracker.trackEvent("Garden", "location", newGarden.getLocality(), 0);
-		
+
 		return newGarden.getId();
 	}
 
@@ -76,39 +82,60 @@ public class GardenManager {
 		changeDatabase(position);
 	}
 
-	private void populateVendorSeed(boolean localStore) {
-		GotsConnector connector;
-		if (!localStore)
-			connector = new SimpleConnector();
-		else
-			connector = new LocalConnector(mContext);
-		List<BaseSeedInterface> seeds = connector.getAllSeeds();
-
-		VendorSeedDBHelper theSeedBank = new VendorSeedDBHelper(mContext);
-		for (Iterator<BaseSeedInterface> iterator = seeds.iterator(); iterator.hasNext();) {
-			BaseSeedInterface baseSeedInterface = iterator.next();
-			if (theSeedBank.getSeedByReference(baseSeedInterface.getReference()) == null)
-				theSeedBank.insertSeed(baseSeedInterface);
-
-		}
-	}
-
 	public void removeCurrentGarden() {
 		GardenDBHelper helper = new GardenDBHelper(mContext);
 		helper.deleteGarden(getcurrentGarden());
 	}
+
 	public void updateCurrentGarden(GardenInterface garden) {
 		GardenDBHelper helper = new GardenDBHelper(mContext);
 		helper.updateGarden(garden);
 	}
-	public void update(){
-		populateVendorSeed(false);
-		
+
+	public void update() {
+		new RefreshTask().execute(new Object(), false);
+
 	}
 
 	public int getNbGarden() {
 		GardenDBHelper helper = new GardenDBHelper(mContext);
-		
+
 		return helper.getCountGarden();
+	}
+
+	private class RefreshTask extends AsyncTask<Object, Boolean, Long> {
+		@Override
+		protected Long doInBackground(Object... params) {
+
+			GardenManager garden = new GardenManager(mContext);
+			// garden.update();
+			GotsConnector connector;
+			if (!isLocalStore)
+				connector = new SimpleConnector();
+			else
+				connector = new LocalConnector(mContext);
+			List<BaseSeedInterface> seeds = connector.getAllSeeds();
+
+			VendorSeedDBHelper theSeedBank = new VendorSeedDBHelper(mContext);
+			for (Iterator<BaseSeedInterface> iterator = seeds.iterator(); iterator.hasNext();) {
+				BaseSeedInterface baseSeedInterface = iterator.next();
+				if (theSeedBank.getSeedByReference(baseSeedInterface.getReference()) == null)
+					theSeedBank.insertSeed(baseSeedInterface);
+
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Long result) {
+//			VendorSeedDBHelper myBank = new VendorSeedDBHelper(mContext);
+//			ArrayList<BaseSeedInterface> vendorSeeds;
+//			vendorSeeds = myBank.getVendorSeeds();
+
+			//setListAdapter(new ListVendorSeedAdapter(mContext, vendorSeeds));
+			Toast.makeText(mContext, "Updated", 20).show();
+			
+			super.onPostExecute(result);
+		}
 	}
 }
