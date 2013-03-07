3b21f9104ea1ef2b143a6adb1b76996a9cd8cf18
sebastien.fleury <contact@gardening-manager.com>
2013-03-07 01:02:19 +0100

    weather updater

--- src/org/gots/service/BootBroadcastReceiver.java
+++ src/org/gots/service/BootBroadcastReceiver.java
@@ -5,6 +5,8 @@ import java.util.Calendar;
 import org.gots.action.service.ActionNotificationService;
 import org.gots.action.service.ActionTODOBroadcastReceiver;
 import org.gots.preferences.GotsPreferences;
+import org.gots.weather.service.WeatherBroadcastReceiver;
+import org.gots.weather.service.WeatherUpdateService;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -22,9 +24,11 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 	}
 	private void setRecurringAlarm(Context context) {
 		Calendar updateTime = Calendar.getInstance();
-		Intent downloader = new Intent(context, ActionTODOBroadcastReceiver.class);
-		PendingIntent actionTODOIntent = PendingIntent.getBroadcast(context, 0, downloader,
+		
+		Intent actionsBroadcaster = new Intent(context, ActionTODOBroadcastReceiver.class);
+		PendingIntent actionTODOIntent = PendingIntent.getBroadcast(context, 0, actionsBroadcaster,
 				PendingIntent.FLAG_UPDATE_CURRENT);
+		
 		AlarmManager alarms = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
 		if (GotsPreferences.getInstance().isDEVELOPPEMENT())
@@ -33,7 +37,21 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 		else {
 			updateTime.set(Calendar.HOUR_OF_DAY, 20);
 			alarms.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(),
-					AlarmManager.INTERVAL_DAY, actionTODOIntent);
+					AlarmManager.INTERVAL_DAY * 7, actionTODOIntent);
+		}
+		
+		
+		Intent weatherBroadcastIntent = new Intent(context, WeatherBroadcastReceiver.class);
+		PendingIntent weatherUpdateService = PendingIntent.getBroadcast(context, 1, weatherBroadcastIntent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+		if (GotsPreferences.getInstance().isDEVELOPPEMENT())
+			alarms.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(),
+					AlarmManager.INTERVAL_FIFTEEN_MINUTES, weatherUpdateService);
+		else {
+			updateTime.set(Calendar.HOUR_OF_DAY, 20);
+			alarms.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(),
+					AlarmManager.INTERVAL_DAY, weatherUpdateService);
 		}
 	}
 }
