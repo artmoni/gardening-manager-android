93c4be810af38e968f29b9bad0bf05380c0cf3c1
sebastien.fleury <contact@gardening-manager.com>
2013-03-24 23:06:02 +0100

    remove my license from externals files projects

--- src/net/londatiga/android/QuickAction.java
+++ src/net/londatiga/android/QuickAction.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2012 sfleury.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * 
- * Contributors:
- *     sfleury - initial API and implementation
- ******************************************************************************/
 package net.londatiga.android;
 
 import java.util.ArrayList;
