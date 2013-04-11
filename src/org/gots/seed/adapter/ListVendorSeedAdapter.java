6dc127d16dfd7df0a46459ed45b65b07ba00b6e0
sebastien.fleury <contact@gardening-manager.com>
2013-04-11 16:11:17 +0200

    FEATURE: Delete ads from list to put it on top

--- src/org/gots/seed/adapter/ListVendorSeedAdapter.java
+++ src/org/gots/seed/adapter/ListVendorSeedAdapter.java
@@ -42,8 +42,9 @@ public class ListVendorSeedAdapter extends BaseAdapter {
 	private LayoutInflater inflater;
 	private Context mContext;
 	private List<BaseSeedInterface> vendorSeeds;
-	private int nbAds = 0;
-	private int frequencyAds = 4;
+	// private int nbAds = 0;
+	// private int frequencyAds = 4;
+	private View adsView;
 
 	public ListVendorSeedAdapter(Context context, List<BaseSeedInterface> vendorSeeds) {
 		// super(context);
@@ -51,117 +52,107 @@ public class ListVendorSeedAdapter extends BaseAdapter {
 		mContext = context;
 		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		if (!GotsPreferences.getInstance(mContext).isPremium())
-			nbAds = vendorSeeds.size() / frequencyAds + 1;
-
 		Collections.sort(vendorSeeds, new ISeedSpecieComparator(context));
-		
-	
+
+		GotsAdvertisement ads = new GotsAdvertisement(mContext);
+		adsView = ads.getAdsLayout();
+
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		if (position % frequencyAds == 0 && !GotsPreferences.getInstance(mContext).isPremium()) {
-			GotsAdvertisement ads = new GotsAdvertisement(mContext);
-			convertView = ads.getAdsLayout();
-			return convertView;
-		} else {
-			View vi = convertView;
-			final BaseSeedInterface currentSeed = getItem(position);
-			SeedWidgetLong seedWidgetLong;
-			ActionWidget actionWidget;
- 
-			// if (vi == null)
+
+		View vi = convertView;
+		final BaseSeedInterface currentSeed = getItem(position);
+		SeedWidgetLong seedWidgetLong;
+		ActionWidget actionWidget;
+
+		if (vi == null)
 			vi = inflater.inflate(R.layout.list_seed, null);
 
-			seedWidgetLong = (SeedWidgetLong) vi.findViewById(R.id.idSeedWidgetLong);
-			actionWidget = (ActionWidget) vi.findViewById(R.id.IdSeedAction);
+		seedWidgetLong = (SeedWidgetLong) vi.findViewById(R.id.idSeedWidgetLong);
+		actionWidget = (ActionWidget) vi.findViewById(R.id.IdSeedAction);
 
-			seedWidgetLong.setSeed(currentSeed);
+		seedWidgetLong.setSeed(currentSeed);
 
-			buying = new BuyingAction(mContext);
-			buying.setState(ActionState.NORMAL);
-			actionWidget.setAction(buying);
-			actionWidget.setOnClickListener(new View.OnClickListener() {
+		buying = new BuyingAction(mContext);
+		buying.setState(ActionState.NORMAL);
+		actionWidget.setAction(buying);
+		actionWidget.setOnClickListener(new View.OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					SeedActionInterface action = (SeedActionInterface) buying;
-					action.execute((GrowingSeedInterface) currentSeed);
-					// Toast.makeText(getContext(),
-					// action.getName() + " " + currentSeed.getSpecie() + " " +
-					// currentSeed.getVariety(), 30).show();
-					notifyDataSetChanged();
-				}
-			});
-			// actionWidget.setOnActionItemClickListener(new
-			// ActionWidget.OnActionItemClickListener() {
-			//
-			// @Override
-			// public void onItemClick(ActionWidget source, BaseActionInterface
-			// baseActionInterface) {
-			// SeedActionInterface action = (SeedActionInterface)
-			// baseActionInterface;
-			// action.execute((GrowingSeedInterface) currentSeed);
-			// Toast.makeText(getContext(),
-			// action.getName() + " " + currentSeed.getSpecie() + " " +
-			// currentSeed.getVariety(), 30).show();
-			// notifyDataSetChanged();
-			// }
-			// });
+			@Override
+			public void onClick(View v) {
+				SeedActionInterface action = (SeedActionInterface) buying;
+				action.execute((GrowingSeedInterface) currentSeed);
+				// Toast.makeText(getContext(),
+				// action.getName() + " " + currentSeed.getSpecie() + " " +
+				// currentSeed.getVariety(), 30).show();
+				notifyDataSetChanged();
+			}
+		});
+		// actionWidget.setOnActionItemClickListener(new
+		// ActionWidget.OnActionItemClickListener() {
+		//
+		// @Override
+		// public void onItemClick(ActionWidget source, BaseActionInterface
+		// baseActionInterface) {
+		// SeedActionInterface action = (SeedActionInterface)
+		// baseActionInterface;
+		// action.execute((GrowingSeedInterface) currentSeed);
+		// Toast.makeText(getContext(),
+		// action.getName() + " " + currentSeed.getSpecie() + " " +
+		// currentSeed.getVariety(), 30).show();
+		// notifyDataSetChanged();
+		// }
+		// });
 
-			Calendar sowTime = Calendar.getInstance();
-			if (sowTime.get(Calendar.MONTH) > currentSeed.getDateSowingMin())
-				sowTime.set(Calendar.YEAR, sowTime.get(Calendar.YEAR) + 1);
-			sowTime.set(Calendar.MONTH, currentSeed.getDateSowingMin());
+		Calendar sowTime = Calendar.getInstance();
+		if (sowTime.get(Calendar.MONTH) > currentSeed.getDateSowingMin())
+			sowTime.set(Calendar.YEAR, sowTime.get(Calendar.YEAR) + 1);
+		sowTime.set(Calendar.MONTH, currentSeed.getDateSowingMin());
 
-			Calendar harvestTime = new GregorianCalendar();
-			harvestTime.setTime(sowTime.getTime());
-			harvestTime.add(Calendar.DAY_OF_MONTH, currentSeed.getDurationMin());
+		Calendar harvestTime = new GregorianCalendar();
+		harvestTime.setTime(sowTime.getTime());
+		harvestTime.add(Calendar.DAY_OF_MONTH, currentSeed.getDurationMin());
 
-			
-			seedWidgetLong.setOnLongClickListener(new View.OnLongClickListener() {
+		seedWidgetLong.setOnLongClickListener(new View.OnLongClickListener() {
 
-				@Override
-				public boolean onLongClick(View v) {
-					Intent i = new Intent(mContext, NewSeedActivity.class);
-					i.putExtra("org.gots.seedid", currentSeed.getSeedId());
-					mContext.startActivity(i);
-					return false;
-				}
-			});
+			@Override
+			public boolean onLongClick(View v) {
+				Intent i = new Intent(mContext, NewSeedActivity.class);
+				i.putExtra("org.gots.seedid", currentSeed.getSeedId());
+				mContext.startActivity(i);
+				return false;
+			}
+		});
 
-			return vi;
-		}
+		return vi;
+
 	}
 
 	@Override
 	public void notifyDataSetChanged() {
-		VendorSeedDBHelper myBank = new VendorSeedDBHelper(mContext);
-		vendorSeeds = myBank.getVendorSeeds();
+		// VendorSeedDBHelper myBank = new VendorSeedDBHelper(mContext);
+		// vendorSeeds = myBank.getVendorSeeds();
 
 		super.notifyDataSetChanged();
 	}
 
 	@Override
 	public int getCount() {
-		return vendorSeeds.size() + nbAds;
+		return vendorSeeds.size();
 	}
 
 	@Override
 	public BaseSeedInterface getItem(int position) {
-		if (position % frequencyAds > 0 && !GotsPreferences.getInstance(mContext).isPremium())
-			position = position - (position / frequencyAds + 1);
+
 		return vendorSeeds.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		if (position % frequencyAds != 0 && !GotsPreferences.getInstance(mContext).isPremium()) {
-			position = position - (position / frequencyAds + 1);
-			return vendorSeeds.get(position).getSeedId();
-		} else
-			return vendorSeeds.get(position).getSeedId();
+
+		return vendorSeeds.get(position).getSeedId();
 	}
 
 }
