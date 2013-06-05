cd3003deb17d9c1fc65c366b97564eb688a6698d
sebastien.fleury <contact@gardening-manager.com>
2013-06-06 00:09:54 +0200

    IMPROVE: Store authentication in preferences

--- src/org/gots/preferences/GotsPreferences.java
+++ src/org/gots/preferences/GotsPreferences.java
@@ -1,3 +1,23 @@
+/* *********************************************************************** *
+ * project: org.gots.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : contact at gardening-manager dot com                  *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file 
+ *   Contributors:                                                         *
+ *                - Sebastien FLEURY                                       *
+ *                                                                         *
+ * *********************************************************************** */
 package org.gots.preferences;
 
 import java.util.List;
