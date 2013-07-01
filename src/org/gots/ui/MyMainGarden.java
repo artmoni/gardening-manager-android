f3fd352af2fa42a5724af565c6cc533fa8e170a8
sebastien.fleury <contact@gardening-manager.com>
2013-07-02 00:30:46 +0200

    IMPROVE: display adds on each main screen

--- src/org/gots/ui/MyMainGarden.java
+++ src/org/gots/ui/MyMainGarden.java
@@ -13,12 +13,14 @@ package org.gots.ui;
 import java.util.Random;
 
 import org.gots.R;
+import org.gots.ads.GotsAdvertisement;
 import org.gots.allotment.adapter.ListAllotmentAdapter;
 import org.gots.allotment.sql.AllotmentDBHelper;
 import org.gots.analytics.GotsAnalytics;
 import org.gots.bean.Allotment;
 import org.gots.bean.BaseAllotmentInterface;
 import org.gots.help.HelpUriBuilder;
+import org.gots.preferences.GotsPreferences;
 import org.gots.weather.view.WeatherWidget;
 
 import android.app.AlertDialog;
@@ -26,6 +28,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -37,110 +40,125 @@ import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 public class MyMainGarden extends SherlockActivity {
 
-	private ListAllotmentAdapter lsa;
-	ListView listAllotments;
-	WeatherWidget weatherWidget;
+    private ListAllotmentAdapter lsa;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    ListView listAllotments;
 
-		ActionBar bar = getSupportActionBar();
-		bar.setDisplayHomeAsUpEnabled(true);
-		bar.setTitle(R.string.dashboard_allotments_name);
+    WeatherWidget weatherWidget;
 
-		GotsAnalytics.getInstance(getApplication()).incrementActivityCount();
-		GoogleAnalyticsTracker.getInstance().trackPageView(getClass().getSimpleName());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// GardenManager gm =GardenManager.getInstance();
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setTitle(R.string.dashboard_allotments_name);
 
-		setContentView(R.layout.garden);
+        GotsAnalytics.getInstance(getApplication()).incrementActivityCount();
+        GoogleAnalyticsTracker.getInstance().trackPageView(
+                getClass().getSimpleName());
 
-		lsa = new ListAllotmentAdapter(this);
-		listAllotments = (ListView) findViewById(R.id.IdGardenAllotmentsList);
-		listAllotments.setAdapter(lsa);
-		listAllotments.setDivider(null);
-		listAllotments.setDividerHeight(0);
+        // GardenManager gm =GardenManager.getInstance();
 
-		if (listAllotments.getCount() == 0) {
-			final String classname = getClass().getSimpleName();
-			new AlertDialog.Builder(this).setIcon(R.drawable.help).setTitle(R.string.menu_help_firstlaunch)
-					.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int whichButton) {
+        setContentView(R.layout.garden);
 
-							Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(HelpUriBuilder
-									.getUri(classname)));
-							startActivity(browserIntent);
-						}
-					}).setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int whichButton) {
+        lsa = new ListAllotmentAdapter(this);
+        listAllotments = (ListView) findViewById(R.id.IdGardenAllotmentsList);
+        listAllotments.setAdapter(lsa);
+        listAllotments.setDivider(null);
+        listAllotments.setDividerHeight(0);
 
-							/* User clicked Cancel so do some stuff */
-						}
-					}).show();
-			// Intent intent = new Intent(this, MyMainGardenFirstTime.class);
-			// startActivity(intent);
-		}
+        if (listAllotments.getCount() == 0) {
+            final String classname = getClass().getSimpleName();
+            new AlertDialog.Builder(this).setIcon(R.drawable.help).setTitle(
+                    R.string.menu_help_firstlaunch).setPositiveButton(
+                    R.string.button_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                int whichButton) {
 
-		// listAllotments.setBackgroundDrawable(getResources().getDrawable(R.drawable.help_hut_2));
+                            Intent browserIntent = new Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse(HelpUriBuilder.getUri(classname)));
+                            startActivity(browserIntent);
+                        }
+                    }).setNegativeButton(R.string.button_cancel,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                int whichButton) {
 
-	}
+                            /* User clicked Cancel so do some stuff */
+                        }
+                    }).show();
+            // Intent intent = new Intent(this, MyMainGardenFirstTime.class);
+            // startActivity(intent);
+        }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
+        // listAllotments.setBackgroundDrawable(getResources().getDrawable(R.drawable.help_hut_2));
+        if (!GotsPreferences.getInstance(this).isPremium()) {
+            GotsAdvertisement ads = new GotsAdvertisement(this);
 
-	}
+            LinearLayout layout = (LinearLayout) findViewById(R.id.idAdsTop);
+            layout.addView(ads.getAdsLayout());
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getSupportMenuInflater();
-		inflater.inflate(R.menu.menu_garden, menu);
-		return true;
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			finish();
-			return true;
-		case R.id.new_allotment:
-			BaseAllotmentInterface newAllotment = new Allotment();
-			newAllotment.setName("" + new Random().nextInt());
+    }
 
-			AllotmentDBHelper helper = new AllotmentDBHelper(this);
-			helper.insertAllotment(newAllotment);
-			lsa.notifyDataSetChanged();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.menu_garden, menu);
+        return true;
+    }
 
-			listAllotments.setBackgroundDrawable(getResources().getDrawable(R.drawable.background_simple));
-			return true;
-		case R.id.help:
-			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(HelpUriBuilder.getUri(getClass()
-					.getSimpleName())));
-			startActivity(browserIntent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        case R.id.new_allotment:
+            BaseAllotmentInterface newAllotment = new Allotment();
+            newAllotment.setName("" + new Random().nextInt());
 
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+            AllotmentDBHelper helper = new AllotmentDBHelper(this);
+            helper.insertAllotment(newAllotment);
+            lsa.notifyDataSetChanged();
 
-	@Override
-	protected void onResume() {
-		lsa.notifyDataSetChanged();
-		super.onResume();
-	}
-	
-	
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	super.onActivityResult(requestCode, resultCode, data);
-}
-	@Override
-	protected void onDestroy() {
-		GotsAnalytics.getInstance(getApplication()).decrementActivityCount();
-		super.onDestroy();
-	}
+            listAllotments.setBackgroundDrawable(getResources().getDrawable(
+                    R.drawable.background_simple));
+            return true;
+        case R.id.help:
+            Intent browserIntent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(HelpUriBuilder.getUri(getClass().getSimpleName())));
+            startActivity(browserIntent);
+
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        lsa.notifyDataSetChanged();
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        GotsAnalytics.getInstance(getApplication()).decrementActivityCount();
+        super.onDestroy();
+    }
 }
