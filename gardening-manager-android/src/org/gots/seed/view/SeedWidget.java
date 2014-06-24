c5922e7c6d9537f739f73e6b9fb7da485a5cb72a
sebastien.fleury <contact@gardening-manager.com>
2014-06-25 00:29:21 +0200

    avoid out of memory with decodescalebitmap

--- gardening-manager-android/src/org/gots/seed/view/SeedWidget.java
+++ gardening-manager-android/src/org/gots/seed/view/SeedWidget.java
@@ -96,9 +96,11 @@ public class SeedWidget extends RelativeLayout implements OnClickListener {
                 if (vegetableImageRessource == 0 && mSeed.getSpecie() != null) {
                     File file = new File(mContext.getCacheDir() + "/"
                             + mSeed.getSpecie().toLowerCase().replaceAll("\\s", ""));
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                    image = BitmapFactory.decodeFile(file.getPath(), options);
+                    
+//                    BitmapFactory.Options options = new BitmapFactory.Options();
+//                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+//                    image = BitmapFactory.decodeFile(file.getPath(), options);
+                    image = decodeScaledBitmapFromSdCard(file.getAbsolutePath(), seedView.getWidth(), seedView.getHeight());
                 }
                 return image;
             }
@@ -145,4 +147,42 @@ public class SeedWidget extends RelativeLayout implements OnClickListener {
 
     }
 
+    
+    public static Bitmap decodeScaledBitmapFromSdCard(String filePath,
+            int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
 }
