bc68c8a86170263040a5d4a2061173253a911217
sebastien.fleury <contact@gardening-manager.com>
2014-07-21 21:06:44 +0200

    BUGFIX: java.lang.IndexOutOfBoundsException on previmeteo weatherconditions

--- gardening-manager-android/src/org/gots/weather/provider/previmeteo/PrevimeteoWeatherProvider.java
+++ gardening-manager-android/src/org/gots/weather/provider/previmeteo/PrevimeteoWeatherProvider.java
@@ -10,11 +10,13 @@
  ******************************************************************************/
 package org.gots.weather.provider.previmeteo;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -27,6 +29,7 @@ import org.gots.weather.WeatherSet;
 import org.gots.weather.provider.WeatherCache;
 import org.gots.weather.provider.local.LocalWeatherProvider;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 import android.content.Context;
@@ -83,83 +86,74 @@ public class PrevimeteoWeatherProvider extends LocalWeatherProvider {
         int dayRequested = requestCalendar.get(Calendar.DAY_OF_YEAR);
         int today = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
 
+        Log.d(TAG, "getCondition " + DateFormat.format("MM/dd/yy h:mmaa", requestedDay));
         if (weatherSet == null) {
-
-            try {
-                Log.d(TAG, "getCondition " + DateFormat.format("MM/dd/yy h:mmaa", requestedDay));
-                cache = new WeatherCache(mContext);
-
-                InputStream is = cache.getCacheByURL(url);
-
-                /* Get a SAXParser from the SAXPArserFactory. */
-                SAXParserFactory spf = SAXParserFactory.newInstance();
-                SAXParser sp = spf.newSAXParser();
-
-                /* Get the XMLReader of the SAXParser we created. */
-                XMLReader xr = sp.getXMLReader();
-
-                PrevimeteoErrorHandler error = new PrevimeteoErrorHandler();
-                xr.setContentHandler(error);
-                xr.parse(new InputSource(is));
-                iserror = error.isError();
-
-                if (!iserror) {
-                    // TODO we should not need to get the cache file again, a
-                    // better way exists
-
-                    is = cache.getCacheByURL(url);
-                    /*
-                     * Create a new ContentHandler and apply it to the
-                     * XML-Reader
-                     */
-                    PrevimeteoWeatherHandler gwh = new PrevimeteoWeatherHandler();
-                    xr.setContentHandler(gwh);
-
-                    // InputSource is = new InputSource(url.openStream());
-                    /* Parse the xml-data our URL-call returned. */
-                    xr.parse(new InputSource(is));
-
-                    /* Our Handler now provides the parsed weather-data to us. */
-                    weatherSet = gwh.getWeatherSet();
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "PrevimeteoErrorHandler has return an error " + e.getMessage());
-                iserror = true;
-                return super.getCondition(requestedDay);
-            }
+            downloadWeatherXML();
         }
 
-        WeatherConditionInterface remoteCondition = new WeatherCondition(requestedDay);
+        WeatherConditionInterface weatherCondition = new WeatherCondition(requestedDay);
+        try {
+            if (weatherSet == null)
+                weatherCondition = new WeatherCondition(requestedDay);
+            else if (dayRequested == today)
+                weatherCondition = weatherSet.getWeatherCurrentCondition();
+            else if (dayRequested > today)
+                weatherCondition = weatherSet.getWeatherForecastConditions().get(dayRequested - today);
+            else if (dayRequested < today
+                    && requestCalendar.get(Calendar.YEAR) > Calendar.getInstance().get(Calendar.YEAR))
+                weatherCondition = weatherSet.getWeatherForecastConditions().get(dayRequested + 365 - today);
+        } catch (Exception e) {
+            weatherCondition = super.getCondition(requestedDay);
+        }
+        weatherCondition.setDate(requestCalendar.getTime());
+        weatherCondition.setDayofYear(requestCalendar.get(Calendar.DAY_OF_YEAR));
 
-        if (weatherSet == null)
-            remoteCondition = new WeatherCondition(requestedDay);
-        else if (dayRequested == today)
-            remoteCondition = weatherSet.getWeatherCurrentCondition();
-        else if (dayRequested > today)
-            remoteCondition = weatherSet.getWeatherForecastConditions().get(dayRequested - today);
-        else if (dayRequested < today && requestCalendar.get(Calendar.YEAR) > Calendar.getInstance().get(Calendar.YEAR))
-            remoteCondition = weatherSet.getWeatherForecastConditions().get(dayRequested + 365 - today);
+        return weatherCondition;
 
-        remoteCondition.setDate(requestCalendar.getTime());
-        remoteCondition.setDayofYear(requestCalendar.get(Calendar.DAY_OF_YEAR));
+    }
 
-        // TODO take care of new year
+    protected void downloadWeatherXML() {
+        cache = new WeatherCache(mContext);
 
-        // WeatherConditionInterface localCondition = super.getWeatherByDayofyear(dayRequested);
-        // if (remoteCondition.getCondition() != null) {
-        // remoteCondition.setDate(requestCalendar.getTime());
-        // remoteCondition.setDayofYear(requestCalendar.get(Calendar.DAY_OF_YEAR));
-        //
-        // if (localCondition != null && localCondition.getId() > 0) {
-        // remoteCondition.setId(localCondition.getId());
-        // remoteCondition = super.updateWeather(remoteCondition);
-        // } else {
-        // remoteCondition = super.insertWeather(remoteCondition);
-        // }
-        // } else if (localCondition != null && localCondition.getCondition() != null)
-        // remoteCondition = localCondition;
-            return remoteCondition;
+        InputStream is = cache.getCacheByURL(url);
 
+        try {
+            /* Get a SAXParser from the SAXPArserFactory. */
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser sp;
+            sp = spf.newSAXParser();
+
+            /* Get the XMLReader of the SAXParser we created. */
+            XMLReader xr = sp.getXMLReader();
+
+            PrevimeteoErrorHandler error = new PrevimeteoErrorHandler();
+            xr.setContentHandler(error);
+            xr.parse(new InputSource(is));
+            iserror = error.isError();
+
+            if (!iserror) {
+                // TODO we should not need to get the cache file again, a
+                // better way exists
+
+                is = cache.getCacheByURL(url);
+                /*
+                 * Create a new ContentHandler and apply it to the
+                 * XML-Reader
+                 */
+                PrevimeteoWeatherHandler gwh = new PrevimeteoWeatherHandler();
+                xr.setContentHandler(gwh);
+
+                // InputSource is = new InputSource(url.openStream());
+                /* Parse the xml-data our URL-call returned. */
+                xr.parse(new InputSource(is));
+
+                /* Our Handler now provides the parsed weather-data to us. */
+                weatherSet = gwh.getWeatherSet();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "PrevimeteoErrorHandler has return an error " + e.getMessage());
+            iserror = true;
+        }
     }
 
     public WeatherConditionInterface updateCondition(WeatherConditionInterface weatherCondition, Date day) {
