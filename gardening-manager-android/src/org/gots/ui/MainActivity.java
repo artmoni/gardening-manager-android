606217323350d7258f8d86d56bd268fce0e94b4b
sebastien.fleury <contact@gardening-manager.com>
2014-08-07 11:39:32 +0200

    ActionBar compatibility on device older than 2.1
    getSupportActionBar instead of gestActionBar

--- gardening-manager-android/src/org/gots/ui/MainActivity.java
+++ gardening-manager-android/src/org/gots/ui/MainActivity.java
@@ -100,8 +100,10 @@ public class MainActivity extends AbstractActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, // nav menu toggle icon
-                R.string.app_name, // nav drawer open - description for accessibility
-                R.string.app_name // nav drawer close - description for accessibility
+                R.string.app_name, // nav drawer open - description for
+                                   // accessibility
+                R.string.app_name // nav drawer close - description for
+                                  // accessibility
         ) {
             public void onDrawerClosed(View view) {
                 getSupportActionBar().setTitle(mTitle);
