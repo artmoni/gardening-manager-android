26ee63095a5f5e42174dab4bbb8ca513646cac4f
sebastien.fleury <contact@gardening-manager.com>
2015-06-24 21:59:59 +0200

    seed widget long image take full width

--- gardening-manager-android/src/org/gots/seed/adapter/ListGrowingSeedAdapter.java
+++ gardening-manager-android/src/org/gots/seed/adapter/ListGrowingSeedAdapter.java
@@ -21,6 +21,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ListGrowingSeedAdapter extends BaseAdapter {
@@ -64,6 +66,7 @@ public class ListGrowingSeedAdapter extends BaseAdapter {
                 SeedWidget widget = new SeedWidget(mContext);
                 widget.setSeed(currentSeed);
                 widget.setTag(currentSeed);
+                widget.setLayoutParams(new RelativeLayout.LayoutParams(80, 100));
                 int sdk = android.os.Build.VERSION.SDK_INT;
                 if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
                     widget.setBackgroundDrawable(mContext.getResources().getDrawable(R.drawable.action_selector));
