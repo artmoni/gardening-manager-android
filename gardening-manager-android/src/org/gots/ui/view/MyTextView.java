1a4711d86ba5ef00f0df3929962e53d7aee34600
sebastien.fleury <contact@gardening-manager.com>
2015-07-27 22:45:35 +0200

    set custom font to resume fragments

--- gardening-manager-android/src/org/gots/ui/view/MyTextView.java
+++ gardening-manager-android/src/org/gots/ui/view/MyTextView.java
@@ -12,14 +12,17 @@ public class MyTextView extends TextView {
 
     public MyTextView(Context context) {
         super(context);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "alpha_echo.ttf"));
     }
 
     public MyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/cartoon.ttf"));
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "alpha_echo.ttf"));
     }
 
     public MyTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "alpha_echo.ttf"));
+
     }
 }
