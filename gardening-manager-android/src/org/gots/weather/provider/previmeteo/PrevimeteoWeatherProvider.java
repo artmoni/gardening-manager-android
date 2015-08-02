3de67c26a4749189b28c64bbdd7735e0d37668cb
sebastien.fleury <contact@gardening-manager.com>
2015-08-02 14:59:11 +0200

    bugfix tracked analytics may be null

--- gardening-manager-android/src/org/gots/weather/provider/previmeteo/PrevimeteoWeatherProvider.java
+++ gardening-manager-android/src/org/gots/weather/provider/previmeteo/PrevimeteoWeatherProvider.java
@@ -1,13 +1,15 @@
-/*******************************************************************************
+/**
+ * ****************************************************************************
  * Copyright (c) 2012 sfleury.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- *
+ * <p>
  * Contributors:
- *     sfleury - initial API and implementation
- ******************************************************************************/
+ * sfleury - initial API and implementation
+ * ****************************************************************************
+ */
 package org.gots.weather.provider.previmeteo;
 
 import java.io.InputStream;
@@ -130,7 +132,8 @@ public class PrevimeteoWeatherProvider extends LocalWeatherProvider {
             iserror = true;
             if (e.getMessage() != null) {
                 GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
-                tracker.trackEvent("Weather", e.getMessage(), "" + gotsPreferences.getWeatherApiKey(), 0);
+                if (tracker != null)
+                    tracker.trackEvent("Weather", e.getMessage(), "" + gotsPreferences.getWeatherApiKey(), 0);
             }
             return WEATHER_ERROR_UNKNOWN;
         }
